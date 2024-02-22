36ee062d633bf2a86e6deb41ea5ad3c420f80ec0
tinb2014796 <tinb2014796@student.ctu.edu.vn>
2024-02-22 15:32:08 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/config/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -10,4 +11,16 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 module.exports = app;
